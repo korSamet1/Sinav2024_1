135f516fca735436009aaa5af58d997599bce940
korSamet1 <sametkokor21@gmail.com>
2024-11-07 13:26:51 +0300

    Update KarakterKontrol.cs

--- Assets/KarakterKontrol.cs
+++ Assets/KarakterKontrol.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 
 public class KarakterKontrol : MonoBehaviour
 {
-    // Ad Soyad: 
-    // Öğrenci Numarası: 
+   
 
-
-    // Soru 1: Karakteri yön tuşları ile hareket ettiren kodu, HareketEt fonksiyonu içerisine yazınız.
-    // Soru 2: Karakterin zıplamasını sağlaması beklenen Zipla metodu doğru bir şekilde çalışmıyor, koddaki hatayı düzeltin.
-    // Soru 3: Karakterin 'Engel' tag'ine sahip objeye temas ettiğinde metin objesine "Oyun Bitti!" yazısını yazdırınız.
-    // Soru 4: Karakterin 'Puan' tag'ine sahip objeye temas ettiğinde skoru 1 arttırınız ve metin objesine yazdırınız.
-
-    // Not: Engel ve Puan nesnelerinin isTrigger özelliği aktiftir.
-
-
-    public TMP_Text metin;
+     public TMP_Text metin;
     private Rigidbody2D karakterRb;
 
     public float hiz = 5f;
@@ -31,21 +21,40 @@ public class KarakterKontrol : MonoBehaviour
 
     void Update()
     {
-        // Yazdığınız metodları çağırınız.
+        HareketEt();
+        Zipla();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Soru 3 ve soru 4 burada çözülecek.
+        if (other.gameObject.CompareTag("Engel"))
+        {
+            metin.text = "Oyun Bitti!";
+        }
+        if (other.gameObject.CompareTag("Puan"))
+        {
+            skor++;
+            metin.text = "Skor: " + skor;
+        }
+    }
+
+    void HareketEt()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            karakterRb.AddForce(Vector2.left * (hiz * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            karakterRb.AddForce(Vector2.right * (hiz * Time.deltaTime));
+        }
     }
 
     void Zipla()
     {
-        // Space tuşuna basınca karakter zıplamalı ancak aşağıdaki kod hatalı.
+      
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 ziplamaYonu = new Vector3(UnityEngine.Random.Range(-1f, 1f), 1, UnityEngine.Random.Range(-1f, 1f));
-            karakterRb.AddForce(ziplamaYonu * (ziplamaGucu / 2), ForceMode2D.Impulse);
-        }
-    }
+            karakterRb.AddForce(Vector2.up * (ziplamaGucu / 2), ForceMode2D.Impulse);
+ }
 }
